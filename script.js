93505775cdd89efe1dd2d10c387e244ed5e80f41
mick-lockwood <mick.lockwood87@gmail.com>
2026-04-22 22:51:26 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let size = 9, bW = 3, bH = 3;
 let mode = 'create', selected = null, pencil = false, paused = false, isWon = false, darkMode = false;
-let board = [], timerVal = 0, timerInt = null;
+let board = []; 
+let timerVal = 0;
+let timerInt = null;
+let paused = false;
 let undoStack = [], redoStack = [];
 
 const colors = ['#fff9c4', '#ffecb3', '#ffe0b2', '#ffcdd2', '#f8bbd0', '#e1bee7', '#d1c4e9', '#bbdefb', '#b3e5fc', '#b2ebf2', '#b2dfdb', '#c8e6c9', '#fff176', '#ffd54f', '#ffb74d', '#ef9a9a', '#f48fb1', '#ce93d8'];
