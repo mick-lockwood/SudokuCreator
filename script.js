e76950da827a768f7de22e5a5571e1a1c7d139f8
mick-lockwood <mick.lockwood87@gmail.com>
2026-04-23 11:15:50 +0930

    Update script.js

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ function renderGrid() {
     const container = document.getElementById('grid');
     container.innerHTML = '';
     container.style.gridTemplateColumns = `repeat(${size}, var(--cell-size))`;
-    const gridLine = darkMode ? "#475569" : "#1e293b";
+    const gridLine = darkMode ? "#475569" : "#ffffff";
     document.getElementById('grid-wrapper').style.background = gridLine;
     container.style.background = gridLine;
 
